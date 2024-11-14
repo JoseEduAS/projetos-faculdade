f87f3e2e42c7fac1217a3d961fc2269ebb138a5e
José Eduardo Ayres da Silva <j.ayres.edu09@gmail.com>
2024-11-14 20:24:25 -0300

    projeto atualizado

---
+++ Projeto-SantaMena/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('open');
+}
+
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('open');
+}
